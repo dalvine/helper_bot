39431c24eedee8ff8ca3c2a816f3053872efb9c5
Kozyrev Vladislav <76097937+dalvine@users.noreply.github.com>
2024-04-08 22:01:54 +0300

    intermediate commit

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import TelegramBot from 'node-telegram-bot-api';
+
 import 'dotenv/config.js';
+import { myVCard } from './helpers/contacts.js';
 
 const { ACCESS_KEY, PHONE_NUMBER } = process.env;
 
 const bot = new TelegramBot(ACCESS_KEY, { polling: true });
 
+const getMessageOfQuery = (data) => {
+    if (data === 'About') return 'Обо мне';
+    else return 'Tecn';
+};
+
 bot.onText('/start', (msg) => {
     const chatId = msg.from.id;
-    bot.se;
 
     bot.sendMessage(
         chatId,
@@ -24,10 +30,51 @@ bot.onText('/start', (msg) => {
 
 bot.onText('Контакты', (msg) => {
     const chatId = msg.chat.id;
-    const msgId = msg.message_id;
-    bot.deleteMessage(chatId, msgId);
 
     bot.sendContact(chatId, PHONE_NUMBER, 'Vladislav', {
-        last_name: 'Kozyrev'
+        last_name: 'Kozyrev',
+        vcard: myVCard.getFormattedString()
     });
+});
+
+bot.onText('Резюме', (msg) => {
+    const chatId = msg.chat.id;
+
+    bot.sendMessage(chatId, 'Что именно вас интересует?', {
+        reply_markup: {
+            inline_keyboard: [
+                [{ text: 'О себе', callback_data: 'About' }],
+                [{ text: 'Опыт работы', callback_data: 'Work expirence' }],
+                [{ text: 'Стек', callback_data: 'Stack' }],
+                [{ text: 'Образование', callback_data: 'Education' }],
+                [{ text: 'Ин. языки', callback_data: 'Language' }]
+            ]
+        }
+    });
+});
+
+bot.on('callback_query', (query) => {
+    if (query.data.includes('back_to_resume')) {
+        bot.editMessageText('Что именно вас интересует?', {
+            chat_id: query.message.chat.id,
+            message_id: query.message.message_id,
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: 'О себе', callback_data: 'About' }],
+                    [{ text: 'Опыт работы', callback_data: 'Work expirence' }],
+                    [{ text: 'Стек', callback_data: 'Stack' }],
+                    [{ text: 'Образование', callback_data: 'Education' }],
+                    [{ text: 'Ин. языки', callback_data: 'Language' }]
+                ]
+            }
+        });
+    } else {
+        bot.editMessageText(getMessageOfQuery(query.data), {
+            chat_id: query.message.chat.id,
+            message_id: query.message.message_id,
+            reply_markup: {
+                inline_keyboard: [[{ text: 'Назад', callback_data: JSON.stringify({ action: 'back_to_resume' }) }]]
+            }
+        });
+    }
 });
